49a4919c7c752f98d5d9a0fdfc7b435e53fe2754
João Pedro Acordi <88681190+JoaoAcordi@users.noreply.github.com>
2025-09-10 20:01:15 -0300

    Update practiceProblem.js

--- singleton/practiceProblem.js
+++ singleton/practiceProblem.js
@@ -1,9 +1,14 @@
-// Classe Database
 class Database {
   constructor(connectionString) {
+    if (Database._instance) {
+      return Database._instance;
+    }
+
     this.connectionString = connectionString;
     this.id = Math.random();
     console.log(`🔌 Nova conexão criada: ${this.id}`);
+
+    Database._instance = this;
   }
 
   query(sql) {
@@ -11,9 +16,10 @@ class Database {
   }
 }
 
-// Cliente cria várias conexões (mesmo que não precise)
 const db1 = new Database("db://meu-banco");
 db1.query("SELECT * FROM users");
 
 const db2 = new Database("db://meu-banco");
 db2.query("SELECT * FROM products");
+
+console.log(db1 === db2);
